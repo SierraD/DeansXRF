7aba8ddfe8c95d2d26ebfe2d2eb8e9ce4cf24aee
SierraD <ccnd@live.com>
2023-10-20 14:12:29 +0900

    Version Two

--- XRFScanSoftware.py
+++ XRFScanSoftware.py
@@ -9,20 +9,32 @@ import plotly.graph_objects
 
 # Formatting and Information
 st.set_page_config(layout="wide")
-st.title("X-Ray Fluorescence Spectrum Scan Analysis Software") 
-st.text("Written by Sierra Dean \n dean.s@spring8.or.jp \n Last updated: 10.19.2023 1:30 pm")
+st.title("DeansXRF Scan")
+st.header("A fast X-ray fluorescence spectroscopy scan analysis application")
+st.text("Written by Sierra Dean \n dean.s@spring8.or.jp \n Last updated: 10.20.2023 2:00 pm")
     
-st.write("This application endeavors to provide fast XRF analysis from various scan files with a large degree of freedom by offering users analysis tools to accomodate a variety of experimental conditions. To begin, please upload the .csv files obtained from the XRF scan. ")
+st.write("This application endeavors to provide fast XRF analysis from various scan files with a large degree of freedom "
+         "by offering users analysis tools to accomodate a variety of experimental conditions. To begin, please upload the "
+         ".csv files obtained from the XRF scan. ")
 
 # Definitions
 def nearest_index(array, item):
+    """Searches an array for an item known to be contained within the array.
+    
+    Args:
+    array: An array or list of items.
+    item: An item.
+    
+    Returns: The index of array which contains the item.
+    """
     return [idx for idx, value in enumerate(array) if value == item]
 
 # Obtain the .csv files of Counts/Channel 
 files = st.sidebar.file_uploader(":chart_with_upwards_trend: Please upload the XRF Spectrum Scan here:", accept_multiple_files=True)
 Assist = st.sidebar.toggle("Display Help")
 if Assist == True:
-    st.sidebar.warning("Please upload .csv files containing a single column of XRF data, formatted as counts per eV. The first cell should be a header titled 'Counts'. Each file should have a maximum size of 200 MB per file.")
+    st.sidebar.warning("Please upload .csv files containing a single column of XRF data, formatted as counts per eV. "
+                       "The first cell should be a header titled 'Counts'. Each file should have a maximum size of 200 MB per file.")
 
 # AMTEK Emission Data
 Element = ["Zinc", "Calcium"]
@@ -36,6 +48,8 @@ if len(files)>0:
 
 # Formatting
 colL, colML, colMR, colR = st.columns(4)
+colL.subheader("Plotting Options")
+colML.subheader("Analysis Options")
 
 # Determine if log(y) plotting and Elemental Analysis is desired
 LogScale = colL.checkbox("Log Plot")
@@ -43,6 +57,7 @@ ElementAnalysis = colML.checkbox("Elemental Analysis")
 
 # Element Selection
 if ElementAnalysis == True:
+    TopContainer = st.container()
     ElementSelected = st.selectbox("Please choose the desired element", Element)
     SelectedIndex = nearest_index(Element, ElementSelected)
     SelectedEnergy = Energy[SelectedIndex[0]]
@@ -50,7 +65,10 @@ if ElementAnalysis == True:
 
 # Plot formatting
 fig = plotly.express.line()
-fig.update_layout(title="Raw Data")
+fig.update_layout(title="Spectrum Data")
+
+hidelegend = colL.checkbox("Hide Legend")
+largeformat = colL.checkbox("Fullscreen Format")
 
 # Plot the spectrum data
 if len(files)>0:
@@ -80,10 +98,25 @@ if len(files)>0:
         right_boundary = boundaries[1] 
         left_boundary_index = nearest_index(x, left_boundary)
         right_boundary_index = nearest_index(x, right_boundary)
-        fig.add_vrect(x0=x[left_boundary_index[0]], x1=x[right_boundary_index[0]], opacity=0.25, fillcolor="Red", line_width=0)
+        fig.add_vrect(x0=x[left_boundary_index[0]], 
+                      x1=x[right_boundary_index[0]], 
+                      opacity=0.25, 
+                      fillcolor="Red", 
+                      line_width=0)
         
         # Determine if element peak analysis is desired
-        ElementPeak = st.checkbox("Element Peak Analysis")
+        ElementPeak = colML.checkbox("Peak Analysis")
+        HelpBox = colML.toggle("Display Help", key="new")
+        if HelpBox == True:
+            TopContainer.warning("The Elemental Analysis option enables the choice of a emission peak of interest to be selected. "
+                                 "A rectangular region will appear, surrounding the known emission energy, indicating where a peak "
+                                 "should be present within your spectrum if emission of the selected element was observed. The "
+                                 "boundaries of the area can be changed if the scaling of the energy is not perfectly aligned. "
+                                 "Once the boundaries are sufficiently selected, the 'Peak Analysis' box can be checked, which "
+                                 "will find the peak counts of your spectrum(s) within that defined area, and plot the peak "
+                                 "results for each unique scan over a progression plot.")
+        
+        
         if ElementPeak == True:
             # Formatting and user input for scan information
             colL, colR = st.columns(2)
@@ -111,10 +144,19 @@ if len(files)>0:
                             index = nearest_index(section[peaks], max(section[peaks]))
                             peaks = peaks[index[0]]
                     # Plot the peak information
-                    fig2.update_layout(xaxis=dict(title="Energy (eV)"), yaxis=dict(title="Counts at "+str(ElementSelected)+" Peak (Log)"))
-                    fig2.add_trace(plotly.graph_objects.Scatter(x=[int(x[peaks+left_boundary_index[0]])],
+                    fig2.update_layout(xaxis=dict(title="Energy (eV)"), 
+                                       yaxis=dict(title="Counts at "+str(ElementSelected)+" Peak (Log)"))
+                    if largeformat == True:
+                        fig2.add_trace(plotly.graph_objects.Scatter(x=[int(x[peaks+left_boundary_index[0]])],
                                                                 y=[float(section[peaks])], 
-                                                                mode="markers", name=str(files[k].name)))
+                                                                marker=dict(size=10),
+                                                                mode="markers", 
+                                                                name=str(files[k].name)))
+                    else:
+                        fig2.add_trace(plotly.graph_objects.Scatter(x=[int(x[peaks+left_boundary_index[0]])],
+                                                                y=[float(section[peaks])], 
+                                                                mode="markers", 
+                                                                name=str(files[k].name)))
 #                     fig2.add_trace(plotly.graph_objects.Scatter(x=x[left_boundary_index[0]:right_boundary_index[0]+1],
 #                                                                 y=section, 
 #                                                                 name=str(files[k].name)))
@@ -129,10 +171,19 @@ if len(files)>0:
                         index = nearest_index(section[peaks+left_boundary_index[0]], max(section[peaks+left_boundary_index[0]]))
                         peaks = peaks[index]
                     # Plot the peak information
-                    fig2.update_layout(xaxis=dict(title="Energy (eV)"), yaxis=dict(title="Counts at "+str(ElementSelected)+" Peak"))
-                    fig2.add_trace(plotly.graph_objects.Scatter(x=[int(x[peaks+left_boundary_index[0]])],
+                    fig2.update_layout(xaxis=dict(title="Energy (eV)"), 
+                                       yaxis=dict(title="Counts at "+str(ElementSelected)+" Peak"))
+                    if largeformat == True:
+                        fig2.add_trace(plotly.graph_objects.Scatter(x=[int(x[peaks+left_boundary_index[0]])],
                                                                 y=[int(section[peaks+left_boundary_index[0]])], 
-                                                                mode="markers", name=str(files[k].name)))
+                                                                marker=dict(size=10),
+                                                                mode="markers", 
+                                                                name=str(files[k].name)))
+                    else:
+                        fig2.add_trace(plotly.graph_objects.Scatter(x=[int(x[peaks+left_boundary_index[0]])],
+                                                                y=[int(section[peaks+left_boundary_index[0]])], 
+                                                                mode="markers", 
+                                                                name=str(files[k].name)))
 #                     fig2.add_trace(plotly.graph_objects.Scatter(x=x[left_boundary_index[0]:right_boundary_index[0]+1],
 #                                                                 y=section, 
 #                                                                 name=str(files[k].name)))
@@ -142,18 +193,44 @@ if len(files)>0:
                 
                 # If user input for step size has been provided, change the plotting specifics
                 if StepSize != 0:
-                    fig3.add_trace(plotly.graph_objects.Scatter(x=[k*StepSize], y=[all_peaks[k]], mode="markers", name=str(files[k].name)))
+                    if largeformat == True:
+                        fig3.add_trace(plotly.graph_objects.Scatter(x=[k*StepSize], 
+                                                                    y=[all_peaks[k]], 
+                                                                    mode="markers", 
+                                                                    marker=dict(size=10),
+                                                                    name=str(files[k].name)))
+                    else:
+                        fig3.add_trace(plotly.graph_objects.Scatter(x=[k*StepSize], 
+                                                                    y=[all_peaks[k]], 
+                                                                    mode="markers", 
+                                                                    name=str(files[k].name)))
                 else:
-                    fig3.add_trace(plotly.graph_objects.Scatter(x=[k], y=[all_peaks[k]], mode="markers", name=str(files[k].name)))
+                    if largeformat==True:
+                        fig3.add_trace(plotly.graph_objects.Scatter(x=[k], 
+                                                                    y=[all_peaks[k]], 
+                                                                    mode="markers", 
+                                                                    marker=dict(size=10),
+                                                                    name=str(files[k].name)))
+                    else:
+                        fig3.add_trace(plotly.graph_objects.Scatter(x=[k], 
+                                                                    y=[all_peaks[k]], 
+                                                                    mode="markers", 
+                                                                    name=str(files[k].name)))
             
             # Plot the average scan calculated from all available information
             ave_peak = np.average(all_sections, axis=0)
-            fig2.add_trace(plotly.graph_objects.Scatter(x=x[left_boundary_index[0]:right_boundary_index[0]+1],y=ave_peak, name="Average"))
+            fig2.add_trace(plotly.graph_objects.Scatter(x=x[left_boundary_index[0]:right_boundary_index[0]+1],
+                                                        y=ave_peak, 
+                                                        line=dict(color="Black", width=1.5),
+                                                        name="Average"))
             
             # Plot Formatting
             points = np.arange(0, len(files)+1)
             if StepSize != 0:
-                fig3.add_trace(plotly.graph_objects.Scatter(x=points*StepSize, y=all_peaks, opacity=0.5))
+                fig3.add_trace(plotly.graph_objects.Scatter(x=points*StepSize, 
+                                                            y=all_peaks, 
+                                                            opacity=0.5, 
+                                                            line=dict(color="Black", dash="dot")))
                 if Unit != "":
                     fig3.update_layout(xaxis=dict(title="Distance ["+str(Unit)+"]"), 
                                        yaxis=dict(title="Counts at "+str(ElementSelected)+" Peak"))
@@ -161,11 +238,30 @@ if len(files)>0:
                     fig3.update_layout(xaxis=dict(title="Distance [Scan Step Size]"), 
                                        yaxis=dict(title="Counts at "+str(ElementSelected)+" Peak"))
             else:
-                fig3.add_trace(plotly.graph_objects.Scatter(x=points, y=all_peaks, opacity=0.5))
-                fig3.update_layout(xaxis=dict(title="Scan Number"), yaxis=dict(title="Counts at "+str(ElementSelected)+" Peak"))
+                fig3.add_trace(plotly.graph_objects.Scatter(x=points, 
+                                                            y=all_peaks, 
+                                                            opacity=0.5, 
+                                                            line=dict(color="Black", dash="dot", width=1.5)))
+                fig3.update_layout(xaxis=dict(title="Scan Number"), 
+                                   yaxis=dict(title="Counts at "+str(ElementSelected)+" Peak"))
                 
+            if hidelegend == True:
+                fig2.update_layout(showlegend=False)
+            if largeformat == True:
+                fig2.update_xaxes(title_font={"size":30}, tickfont=dict(size=25))
+                fig2.update_yaxes(title_font={"size":30}, tickfont=dict(size=25))
             st.plotly_chart(fig2)
+            if hidelegend == True:
+                fig3.update_layout(showlegend=False)
+            if largeformat == True:
+                fig3.update_xaxes(title_font={"size":30}, tickfont=dict(size=25))
+                fig3.update_yaxes(title_font={"size":30}, tickfont=dict(size=25))
             st.plotly_chart(fig3)
+    if hidelegend == True:
+        fig.update_layout(showlegend=False)
+    if largeformat == True:
+        fig.update_xaxes(title_font={"size":30}, tickfont=dict(size=25))
+        fig.update_yaxes(title_font={"size":30}, tickfont=dict(size=25))
     AfterBoundaries.plotly_chart(fig)
     
 # Formatting and Information
@@ -175,5 +271,6 @@ st.text("XRF Values provided by AMETEK \n https://www.amptek.com/resources/perio
 UpdateLog = st.checkbox(":clipboard: Would you like to view the update log?")
 if UpdateLog == True:
     st.balloons()
+    st.text("10.20.2023 2:00 pm, Ploting and analysis format changes.")
     st.text("10.19.2023 1:30 pm, Added the element analysis plots and added to github.")
     st.text("10.17.2023 9:00 am, Wrote the software")
